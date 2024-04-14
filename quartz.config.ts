fed03c0aad26b1633188a2e0809458a06f143a18
wtljhc <xzy1151564287@gmail.com>
2024-04-14 21:33:39 +0800

    Quartz sync: Apr 14, 2024, 9:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Noto Serif Simplified Chinese",
+        body: "Noto Sans SC",
         code: "Fira Code",
       },
       colors: {
